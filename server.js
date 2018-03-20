52581bd42b0649ea2f54911aade9a550df243d3b
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-03-20 11:28:47 +0000

    17.5

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.use(function(req, res, next) {
+	console.log("Middleware");
+	next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.get('/store', function (req, res) {
+    res.send('This is the store');
+});
+
+
+app.listen(3000);
+
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry, We fuc*ed up or you're asking for too much...");
+});
