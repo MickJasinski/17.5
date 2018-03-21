9ccc81fd533ad4fe5fa1309798da2bb86f6b79c7
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-03-21 09:30:59 +0000

    Remove .DS_Store and format code

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello world');
+app.get("/", function(req, res) {
+	res.send("Hello world");
 });
 
-app.get('/store', function (req, res) {
-    res.send('This is the store');
+app.get("/store", function(req, res) {
+	res.send("This is the store");
 });
-
 
 app.listen(3000);
 
 app.use(function(req, res, next) {
-  res.status(404).send("Sorry, We fuc*ed up or you're asking for too much...");
+	res.status(404).send("Sorry, We fuc*ed up or you're asking for too much...");
 });
